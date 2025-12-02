6276b3b85aed8aa25fff0842177f4c707dc1bcb2
ali-eltaweel <ali.kamel.dev@gmail.com>
2025-12-02 05:03:01 +0200

    Create relationships

--- src/Relationship.php
+++ src/Relationship.php
@@ -2,11 +2,15 @@
 
 namespace Neo4jGRM;
 
+use Closure;
 use Generator;
 use InvalidArgumentException;
 use Laudis\Neo4j\Databags\SummarizedResult;
 use Laudis\Neo4j\Types\CypherMap;
 use Laudis\Neo4j\Types\Node;
+use Laudis\Neo4j\Types\Relationship as TypesRelationship;
+use Neo4jQueryBuilder\Clauses\Match_;
+use Neo4jQueryBuilder\Clauses\Where;
 use Neo4jQueryBuilder\QueryBuilder;
 use Neo4jQueryBuilder\RelationshipBuilder;
 
@@ -41,7 +45,7 @@ class Relationship extends Entity {
     public function __get(string $property): mixed {
 
         if ($property === 'startingNode') {
-            
+    
             return $this->startingNode;
         }
 
@@ -63,15 +67,23 @@ class Relationship extends Entity {
         parent::__set($property, $value);
     }
 
-    public static final function get(int|string|null $id = null, array $properties = [], ?int $skip = null, ?int $limit = null): Generator {
+    public static final function get(
+        
+        int|string|null $id = null,
+        array $properties = [],
+        ?int $skip = null,
+        ?int $limit = null,
+        null|Label|Closure $start = null,
+        null|Label|Closure $end = null,
+    ): Generator {
 
         $query = new QueryBuilder();
 
         $query->match()->relationship(function(RelationshipBuilder $relationship) use ($properties) {
 
             $relationship->alias('rel')->label(static::getLabel())->properties($properties);
-            $relationship->from()->alias('from');
-            $relationship->to()->alias('to');
+            $relationship->from()->alias('start')->label((new static([]))->startingLabel()::getLabel());
+            $relationship->to()->alias('end')->label((new static([]))->endingLabel()::getLabel());
         });
 
         if (!is_null($id)) {
@@ -79,13 +91,39 @@ class Relationship extends Entity {
             $query->where()->condition()->name('id(rel)')->operator('=')->value($id);
         }
 
-        $query->return()->elements(['from', 'rel', 'to']);
+        if (!is_null($start ?? $end)) {
+
+            $query->where(function(Where $where) use ($start, $end) {
+
+                if ($start instanceof Label) {
+
+                    $where->condition()->name('id(start)')->operator('=')->value($start->id);
+                }
+
+                if (is_callable($start)) {
+
+                    $start($where);
+                }
+
+                if ($end instanceof Label) {
+
+                    $where->condition()->name('id(end)')->operator('=')->value($end->id);
+                }
+                
+                if (is_callable($end)) {
+
+                    $end($where);
+                }
+            });
+        }
+
+        $query->return()->elements(['start', 'rel', 'end']);
 
         if (!is_null($skip))  $query->skip($skip);
         if (!is_null($limit)) $query->limit($limit);
 
         /** @var SummarizedResult $result */
-        $result = static::getClient()->run($query);
+        $result = static::getClient()->run($query, $query->getParameters());
 
         /** @var CypherMap $record */
         foreach ($result as $record) {
@@ -93,11 +131,11 @@ class Relationship extends Entity {
             /** @var Node $relationship */
             $relationship = $record->get('rel');
             
-            /** @var Node $from */
-            $from = $record->get('from');
+            /** @var Node $start */
+            $start = $record->get('start');
 
-            /** @var Node $to */
-            $to = $record->get('to');
+            /** @var Node $end */
+            $end = $record->get('end');
 
             $instance = new static([
                 'id' => $relationship->getId(),
@@ -107,20 +145,91 @@ class Relationship extends Entity {
             if (!is_null($labelClass = $instance->startingLabel())) {
 
                 $instance->startingNode = new $labelClass([
-                    'id' => $from->getId(),
-                    ...$from->getProperties()->toArray()
+                    'id' => $start->getId(),
+                    ...$start->getProperties()->toArray()
                 ]);
             }
 
             if (!is_null($labelClass = $instance->endingLabel())) {
 
                 $instance->endingNode = new $labelClass([
-                    'id' => $to->getId(),
-                    ...$to->getProperties()->toArray()
+                    'id' => $end->getId(),
+                    ...$end->getProperties()->toArray()
                 ]);
             }
 
             yield $instance;
         }
+    }
+
+    public static final function first(int|string|null $id = null, array $properties = [], null|Label|Closure $start = null, null|Label|Closure $end = null): ?static {
+
+        foreach (static::get(id: $id, properties: $properties, limit: 1, start: $start, end: $end) as $record) {
+
+            return $record;
+        }
+
+        return null;
+    }
+
+    public static final function create(Label|Closure $start, Label|Closure $end): static {
+
+        $query = new QueryBuilder();
+
+        $query->match(function(Match_ $match) {
+            $match->node()->alias('start');
+            $match->node()->alias('end');
+        });
+
+        $query->where(function(Where $where) use ($start, $end) {
+
+            if ($start instanceof Label) {
+
+                $where->condition()->name('id(start)')->operator('=')->value($start->id);
+            } else {
+
+                $start($where);
+            }
+
+            if ($end instanceof Label) {
+
+                $where->condition()->name('id(end)')->operator('=')->value($end->id);
+            } else {
+
+                $end($where);
+            }
+        });
+
+        $query->create()->relationship(function(RelationshipBuilder $rel) {
+
+            $rel->alias('rel');
+            $rel->from()->alias('start');
+            $rel->label(static::getLabel());
+            $rel->to()->alias('end');
+        });
+
+        $query->return()->element('rel');
+
+        /** @var SummarizedResult $result */
+        $result = static::getClient()->run($query, $query->getParameters());
+
+        /** @var CypherMap $cypherMap */
+        $cypherMap = $result->first();
+
+        /** @var TypesRelationship $relationship */
+        $relationship = $cypherMap->get($cypherMap->key());
+
+        /** @var CypherMap $properties */
+        $properties = $relationship->getProperties();
+
+        return new static([
+            'id' => $relationship->getId(),
+            ...$properties->toArray()
+        ]);
+    }
+
+    public static final function firstOrCreate(Label|Closure $start, Label|Closure $end): ?static {
+
+        return static::first(start: $start, end: $end) ?? static::create(start: $start, end: $end);
     }
 }
